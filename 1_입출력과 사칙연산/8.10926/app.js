01fdf05ae41beac4c91fff03bbefab20e5e0228e
2pandi <coder.2pandi@gmail.com>
2022-08-17 07:23:31 +0900

    pass: 1_8.10926

---
+++ 1_입출력과 사칙연산/8.10926/app.js
@@ -0,0 +1,7 @@
+//한 줄인 경우
+let fs = require('fs');
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString();
+
+console.log(input + '??!');
+
