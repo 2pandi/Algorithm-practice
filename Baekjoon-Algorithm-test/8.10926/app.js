33682bc876086510851054b1743ded51b96aea03
2pandi <coder.2pandi@gmail.com>
2022-09-16 21:55:30 +0900

    Reset files

--- Baekjoon-Algorithm-test/8.10926/app.js
+++
@@ -1,7 +0,0 @@
-//한 줄인 경우
-let fs = require('fs');
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString();
-
-console.log(input + '??!');
-
