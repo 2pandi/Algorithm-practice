d6fa1dba6652fd7ad793a204d76f5ec7c0ee63fa
2pandi <coder.2pandi@gmail.com>
2023-01-26 11:59:50 +0900

    BST(BFS, DFS)설명 추가

--- Algorithm-and-Data-structure/Binary-Search-tree.js
+++ Algorithm-and-Data-structure/Binary-Search-tree.js
@@ -80,6 +80,8 @@ class BinarySearchTree {
     return data;
   }
 
+  // 전위 탐색
+  // [10, 5, 2, 7, 13, 11, 16]
   DFSPreOrder() {
     let current = this.root,
       data = [];
@@ -92,6 +94,8 @@ class BinarySearchTree {
     return data;
   }
 
+  // 후위 탐색
+  // [2, 7, 5, 11, 16, 13, 10]
   DFSPostOrder() {
     let current = this.root,
       data = [];
@@ -104,13 +108,15 @@ class BinarySearchTree {
     return data;
   }
 
+  // 정위 탐색
+  // [2, 5, 7, 10, 11, 13, 16]
   DFSInOrder() {
     let current = this.root,
       data = [];
     function traverse(node) {
-      if (node.left) traverse(node.left);
+      node.left && traverse(node.left); // if문을 &&연산자로 대체 가능
       data.push(node.value);
-      if (node.right) traverse(node.right);
+      node.right && traverse(node.right);
     }
     traverse(current);
     return data;
@@ -132,4 +138,16 @@ tree.insert(7);
  *      10
  *   5      13
  * 2   7  11  16
+ */
+
+/** BFS와 DFS
+ * BFS와 DFS 모두 모든 노드를 한번씩 방문하는 것이므로 시간복잡도는 동일하다.
+ * 하지만 트리의 구조에 따라 공간복잡도가 달라질 수 있다.
+ * 너비가 넓은 트리는 DFS가, 깊이가 깊은 트리는 BFS가 공간복잡도 측면에서 유리할 수 있다.
+ */
+
+/** 전위, 후위, 정위 탐색
+ * 이진검색트리로 한정하여 봤을 때
+ * 정위 탐색으로 순회하면 오름차순으로 정렬된다.
+ * 전위 탐색으로 순회하면 트리를 복사하거나 평탄화하여 저장해놓고 나중에 다시 연쇄구조로 만들기 수월할 수 있다.(맨 앞이 root)
  */
